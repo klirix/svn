76b895c6e2ab7770ec39c4bf8e9390c8573bfc09
UniMates <klirikus@gmail.com>
2019-03-20 22:16:25 +0300

    add signature check

--- lib/streamlabs_intro_web/controllers/api_controller.ex
+++ lib/streamlabs_intro_web/controllers/api_controller.ex
@@ -1,6 +1,8 @@
 defmodule StreamlabsIntroWeb.ApiController do
   use StreamlabsIntroWeb, :controller
 
+  plug :check_hash when action in [:send]
+
   def confirm(conn, %{ "hub.challenge" => ch }), do: text(conn, ch)
 
   def send(conn, %{"event" => event, "id" => id, "data" => data}) do
@@ -23,5 +25,21 @@ defmodule StreamlabsIntroWeb.ApiController do
         end
     end
     conn |> text("ok")
+  end
+
+  defp check_hash(conn, _) do
+    {:ok, body, _} =  conn |> read_body()
+    [hash] = conn |> get_req_header("x-hub-signature")
+    hash = String.downcase(hash)
+    secret = Application.get_env(:streamlabs_intro, StreamlabsIntroWeb.Endpoint)[:secret_key_base]
+    expected_hash = String.downcase("sha256="+(:crypto.hmac(:sha256, secret, body) |> Base.encode16()))
+    if hash == expected_hash do
+      conn
+    else
+      conn
+      |> put_status(401)
+      |> text("failed check")
+      |> halt()
+    end
   end
 end
